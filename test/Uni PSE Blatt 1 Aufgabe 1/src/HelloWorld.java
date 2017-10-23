072f418eb7cf3b9e6fa43d925b256d4bd7c24fc1
apcnc <fchluca@web.de>
2017-10-23 22:28:47 +0200

    First Sheet

---
+++ test/Uni PSE Blatt 1 Aufgabe 1/src/HelloWorld.java
@@ -0,0 +1,23 @@
+
+public class HelloWorld {
+	public static void main(String[] args){
+		String helloWorld = "Hallo Welt!";
+		String javaIsAnIsland = "Java ist auch eine Insel";
+		
+		System.out.println("Hallo Welt!" );
+		System.out.println("Java ist \n auch eine Insel");
+		System.out.println(helloWorld);
+		System.out.println(javaIsAnIsland);
+		
+		/*	System.out.println() macht einen Zeilenumbrach nach der Eingabe
+		 *  System.out.print() macht keinen
+		 *  
+		 *  Zeilenumbrüche in String mit \n 
+		 *  
+		 *  Es ist sinnvoll variablen für alle Werte zu verwenden die nicht fest sind
+		 */
+	}
+	
+}
+
+
